6d426ed85438ba41902b50b5e6569d0a46ef60b1
sunyaqin <sunyaqin@ehousechina.com>
2017-07-18 18:35:56 +0800

    增加依赖项

---
+++ dev-server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var path = require('path')
+var webpack = require('webpack')
+// var proxyMiddleware = require('http-proxy-middleware')
+var webpackConfig = require('./webpack.config.js')
+
+var app = express()
+var compiler = webpack(webpackConfig)
+
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
+  publicPath: webpackConfig.output.publicPath,
+  quiet: true
+})
+
+var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+  log: () => {}
+})
+// force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
+
+app.use(devMiddleware)
+app.listen(8080)
